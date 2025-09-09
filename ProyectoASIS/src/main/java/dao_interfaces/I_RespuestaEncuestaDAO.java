a92ebf3b77afcf1468f46344c9d0fe7424d20979
vals999 <valentinsantana2010@hotmail.com>
2025-09-09 16:19:14 -0300

    filtros agregados a la vista mapa

--- ProyectoASIS/src/main/java/dao_interfaces/I_RespuestaEncuestaDAO.java
+++ ProyectoASIS/src/main/java/dao_interfaces/I_RespuestaEncuestaDAO.java
@@ -6,7 +6,6 @@ import model.RespuestaEncuesta;
 public interface I_RespuestaEncuestaDAO extends I_GenericDAO<RespuestaEncuesta, Long>{
     List<RespuestaEncuesta> obtenerNoBorrados();
     List<String> obtenerRespuestasUnicasPorPreguntaCodigo(String preguntaCodigo);
-    List<RespuestaEncuesta> obtenerRespuestasPorPreguntaYValor(String preguntaCodigo, String valor);
     List<RespuestaEncuesta> obtenerRespuestasPorPreguntaCodigo(String preguntaCodigo);
     RespuestaEncuesta obtenerRespuestaPorEncuestaYPregunta(Long encuestaId, Long preguntaId);
     RespuestaEncuesta obtenerRespuestaPorEncuestaYPreguntaCsv(Long encuestaId, String preguntaCsv);
